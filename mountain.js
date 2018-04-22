589a33c3f9f4327e9662bbadfe048cbe6047fc55
Fen Dweller <chemicalcrux@gmail.com>
2018-04-22 08:46:22 -0400

    nut

--- mountain.js
+++ mountain.js
@@ -91,7 +91,13 @@ function MountainWyrm() {
     else if (this.flags.state == "stomach")
       return ["You give one last heave...and digest."];
     else if (this.flags.state == "balls")
-      return ["You fall limp in " + this.description("the") + "'s balls."];
+      return ["The wyrm's thick, clingy cum proves too much for your body to bear. Slowly, surely, your struggles begin to fade - melting muscles dwindling your strength away to nothing. Deep, satisfied growls reverberate through your captor's massive seed-soaked prison...the last thing you perceive before everything goes dark.",
+      newline,
+      "Your body takes several hours to melt, breaking down and dissolving into a lake of hot, churning cum. The weight is enough to pin the greedy beast to the ground, and there he lays - panting, groaning, already forgetting the struggle he made to consume you and digest you alive. All the beast cared about now was chasing that <i>release.</i>",
+      newline,
+      "The bubble-churn of flesh and bone becoming seed finally ends, and the scaly predator lets out a triumphant roar. He shifts onto his back, curling up to raise his snout to the tip of his foot-long shaft. Draconic jaws part and a slick tongue slithers out...and he begins to lick. Every stroke of wet muscle sends a jolt of ecstasy up his spine. All too eager to release its pent-up flood, his cock throbs and twitches, engoring with blood and spurting out gobs of precum with each quick, forceful <i>slurp</i>. His lust builds for a half-minute, and then...",
+      newline,
+      "The first shot of acum arcs through the air. A gallon of rich, clingy wyrm seed splatters against a cliff face, raining down on the dusty ground. The next lands squarely in the wyrm's jaws, gushing down his throat and stuffing his belly with what was once <i>you</i>...and coating the rest of his body, too. He roars and thrashes, every orasmic clench of muscle spraying pint after pint of his cum. The display drags on for an entire minute, slowly reducing in intensity - and then comes the finale, as a bulge rushes up his cock and erupts into the world. Your skull rockets from that predatory cock, bouncing off the wall and clattering to the ground amidst an ankle-deep pool of cum."];
   };
 }
 
