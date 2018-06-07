9f5a91506da4e4f4e6dca3dea971c59829d436f7
Fen Dweller <chemicalcrux.org>
2018-06-06 23:05:05 -0400

    Added really basic stuff for Arokha

---
+++ aronai.js
@@ -0,0 +1,33 @@
+function Arokha() {
+  Creature.call(this, "Arokha", 15, 30, 25);
+
+  this.hasName = true;
+
+  this.description = function() { return "Arokha"; };
+
+  this.attacks = [];
+
+  this.flags.state = "combat";
+
+  let attacker = this;
+
+  this.attacks.push({
+      attackPlayer: function(defender){
+        defender.health = -100;
+        return ["Arokha eats you. burp."];
+      },
+      requirements: [
+        function(attacker, defender) {
+          return attacker.flags.state == "combat";
+        }
+      ],
+      priority: 1,
+      weight: function(attacker, defender) { return 1 }
+    });
+
+  this.backupAttack = new pass(this);
+
+  this.playerAttacks = [];
+
+  this.playerAttacks.push(pass);
+}
